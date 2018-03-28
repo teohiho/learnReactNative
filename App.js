215e129a1a19e6ea53f8f0f58660bb41189f478e
Thu Hiền Hồ <thuhien191197@gmail.com>
2018-03-28 14:10:48 +0700

    form-login

--- App.js
+++ App.js
@@ -1,29 +1,90 @@
+
 import React, { Component } from 'react';
-import { View, TextInput, AppRegistry, Image } from 'react-native';
- 
-export default class Background extends Component{
+import {
+  AppRegistry,
+  Image,
+  Text,
+  View,
+  Dimensions,
+  TextInput,
+  Button,
+  Alert,
+} from 'react-native';
+
+const { width, height } = Dimensions.get('window')
+
+const remote = 'https://upload.wikimedia.org/wikipedia/commons/7/74/Image_Background.png';
+
+export default class BackgroundImage extends Component {
   constructor(props){
     super(props);
     this.state = {text: ''};
   }
 
-  render(){
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/7/74/Image_Background.png'
-    };
-    return(
+  render() {
+    const resizeMode = 'cover';
+    const text = 'Login';
 
-      <View style={{flex:1}}>
-        <Image style={{flex:1}} source={pic}  />
-        <View>
-          <TextInput style={{height: 40}} placeholder="User"/>
+    return (
+      <View
+        style={{
+            flex: 1,
+            backgroundColor: 'red',
+        }}
+
+      >
+        <Image
+          style={{
+            flex: 1,
+            resizeMode,
+            position: 'absolute',
+            width: width,
+            height: height,
+            justifyContent: 'center',
+          }}
+          source={{ uri: remote }}
+        />
+        <View style={{marginTop: 60}}>
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 30,
+              padding: 40,
+              color: 'white'
+            }}
+          >
+            {text}
+          </Text>
         </View>
+       
+        <View style={{marginBottom: 30,marginLeft: 20,marginRight: 20,marginTop: 20}}>
+          <TextInput
+            style={{height: 50, padding: 10, color: 'white',}}
+            placeholder="Username"
+          />
+          <TextInput
+            style={{height: 50, padding: 10, color: 'white'}}
+            placeholder="Password"
+          />
+        </View>
+       
+        <View style={{marginBottom: 30,marginLeft: 20,marginRight: 20,marginTop: 20}}>
+          <Button 
+          style={{marginBottom: 30,marginLeft: 20,}}
+          onPress={()=> {
+            Alert.alert('Sucsess!!');
+          }}
+          title="Login"
+          />  
+        </View>
+        
       </View>
-
     );
   }
 }
 
 
 
-AppRegistry.registerComponent('AwesomeProject', () => Background );
+
+
+AppRegistry.registerComponent('BackgroundImage', () => BackgroundImage);
